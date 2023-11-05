168dc32443adba43d15c821c612a5c30c88167a7
Mionka522 <your.artist.alex@mail.ru>
2023-11-05 19:45:18 +0400

    .

--- src/main/java/com/my/lib/dao/BookDAO.java
+++ src/main/java/com/my/lib/dao/BookDAO.java
@@ -1,4 +1,22 @@
 package com.my.lib.dao;
 
+import com.my.lib.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class BookDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
+    }
 }
